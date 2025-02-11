7cb7e8015bde2a2a3c3be9a8eb7b98192f6df697
Jiří Chroust <XCHRJ016@studenti.czu.cz>
2025-02-11 22:51:58 +0100

    commit

--- ModularDieselApplication.Application/obj/Debug/net8.0/ModularDieselApplication.Application.AssemblyInfo.cs
+++ ModularDieselApplication.Application/obj/Debug/net8.0/ModularDieselApplication.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModularDieselApplication.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f330b8b2ed6a315ccbf4513dd235958e0d8c41f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModularDieselApplication.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModularDieselApplication.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
