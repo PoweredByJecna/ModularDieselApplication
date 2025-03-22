afcc2465bd7ae451ba851e8a7a03d6d5639276b4
Jiří Chroust <jirkachroust4589@email.cz>
2025-03-22 23:05:41 +0100

    commit

--- tests/ModularDieselApplication.Tests/obj/Debug/net8.0/ModularDieselApplication.Tests.AssemblyInfo.cs
+++ tests/ModularDieselApplication.Tests/obj/Debug/net8.0/ModularDieselApplication.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModularDieselApplication.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ae3710844ce3d59b8e22a6dd6987d199dbc291e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06f6b94925456a10a57b09f82e876b966a6c2ae9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModularDieselApplication.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModularDieselApplication.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
